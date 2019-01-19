fc2d7d9a29b58595361ae43e6f109237696b443d
ekapopev <ekapope.v@gmail.com>
2019-01-19 08:27:32 +0100

    Update Combine_CSVs.py

--- Combine_CSVs.py
+++ Combine_CSVs.py
@@ -4,6 +4,7 @@ Created on Sat Jan 12 06:56:16 2019
 
 @author: Chris
 """
+#credited:
 #https://stackoverflow.com/questions/9234560/find-all-csv-files-in-a-directory-using-python/12280052
 import glob
 
@@ -16,7 +17,7 @@ import glob
 
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-print(all_filenames)
+#print(all_filenames)
 
 #combine all files in the list
 import pandas as pd
